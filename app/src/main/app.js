4763d2bb5262fd39bc8f5659a8d8dabb3b860aa6
Aidan Smith <aidan@cryptonumerics.com>
2020-04-07 15:27:17 -0400

    Added all elements required to test new error from the main project

--- app/src/main/app.js
+++ app/src/main/app.js
@@ -23,7 +23,6 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
-            preload: path.join(__dirname, './res/sentry.js'),
         }
     });
 
@@ -68,3 +67,66 @@ app.on('activate', function() {
 app.on('window-all-closed', () => {
  app.quit();
 });
+
+const createMenu = () => {
+	const application = {
+		label: 'Application',
+		submenu: [{
+				label: 'About Application',
+				role: 'about',
+			},
+			{
+				type: 'separator',
+			},
+			{
+				label: 'Quit',
+				accelerator: 'Command+Q',
+				click: () => {
+					electron.app.quit();
+				},
+			},
+		],
+	};
+
+	const edit = {
+		label: 'Edit',
+		submenu: [{
+				label: 'Undo',
+				accelerator: 'CmdOrCtrl+Z',
+				role: 'undo',
+			},
+			{
+				label: 'Redo',
+				accelerator: 'Shift+CmdOrCtrl+Z',
+				role: 'redo',
+			},
+			{
+				type: 'separator',
+			},
+			{
+				label: 'Cut',
+				accelerator: 'CmdOrCtrl+X',
+				role: 'cut',
+			},
+			{
+				label: 'Copy',
+				accelerator: 'CmdOrCtrl+C',
+				role: 'copy',
+			},
+			{
+				label: 'Paste',
+				accelerator: 'CmdOrCtrl+V',
+				role: 'paste',
+			},
+			{
+				label: 'Select All',
+				accelerator: 'CmdOrCtrl+A',
+				role: 'selectAll',
+			},
+		],
+	};
+
+	const template = [application, edit];
+
+	electron.Menu.setApplicationMenu(electron.Menu.buildFromTemplate(template));
+};
